53120dac2ff50567807c837587627d981eb95d0a
Анатолий Соловьев <bakalavr12@mail.ru>
2025-04-14 15:04:42 +0500

    Revert "Full MVP"
    This reverts commit 83615d5a662cb7de35878765d8e56d04a146d68a.

--- CheckStatus/main.cpp
+++ CheckStatus/main.cpp
@@ -5,9 +5,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     CheckStatus w;
-
-	w.setWindowIcon(QIcon("icon.png"));
-	w.setWindowTitle("CheckStatus by Solovev");
     w.show();
     return a.exec();
 }
